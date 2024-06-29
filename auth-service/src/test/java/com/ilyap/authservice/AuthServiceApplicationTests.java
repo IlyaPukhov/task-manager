d06a3f230ca23d5340b732b0fb2b24f2433dc450
Ilya Pukhov <dinamond2003@gmail.com>
2024-06-29 04:46:23 +0300

    add microservices templates

---
+++ auth-service/src/test/java/com/ilyap/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilyap.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ common-logging-starter/src/test/java/com/ilyap/commonloggingstarter/CommonLoggingStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilyap.commonloggingstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonLoggingStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
