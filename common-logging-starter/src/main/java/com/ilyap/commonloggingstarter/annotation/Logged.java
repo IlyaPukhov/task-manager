db225ad6a8e4b8a4e22da4d769d9c87da309807b
Ilya Pukhov <dinamond2003@gmail.com>
2024-06-29 08:44:32 +0300

    javadoc for logging starter

--- common-logging-starter/src/main/java/com/ilyap/commonloggingstarter/annotation/Logged.java
+++ common-logging-starter/src/main/java/com/ilyap/commonloggingstarter/annotation/Logged.java
@@ -5,9 +5,27 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * The {@code @Logged} is used to mark type or method that should be logged for execution information.
+ * When applied at the class level, all methods within the class will be considered for logging.
+ * When applied at the method level, only the annotated method will be logged.
+ *
+ * <p>Usage:
+ * <pre>
+ *  // Apply at the class level to log all methods
+ * {@literal @}Logged
+ *  public class TaskServiceImpl implements TaskService {
+ *      ...
+ *  }
+ *
+ *  // Apply at the method level to log a concrete method
+ * {@literal @}Logged
+ *  public Page<TaskReadDto> findAllByUsername(String username, Pageable pageable) {
+ *      ...
+ *  }
+ * </pre>
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE, ElementType.METHOD})
 public @interface Logged {
-
-    String value() default "";
 }
--- common-logging-starter/src/main/java/com/ilyap/commonloggingstarter/config/LoggingAutoConfiguration.java
+++ common-logging-starter/src/main/java/com/ilyap/commonloggingstarter/config/LoggingAutoConfiguration.java
@@ -3,13 +3,28 @@ package com.ilyap.commonloggingstarter.config;
 import com.ilyap.commonloggingstarter.aop.MethodLoggingAspect;
 import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * {@link EnableAutoConfiguration Auto-configuration} for Logging.
+ * <p>
+ * Auto-configuration classes are regular Spring {@link Configuration @Configuration}
+ * beans. Generally auto-configuration beans are {@link Conditional @Conditional} beans (most often using
+ * {@link ConditionalOnClass @ConditionalOnClass}, {@link ConditionalOnProperty @ConditionalOnProperty} and
+ * {@link ConditionalOnMissingBean @ConditionalOnMissingBean} annotations).
+ * </p>
+ *
+ * @see Conditional
+ * @see ConditionalOnClass
+ * @see ConditionalOnProperty
+ */
 @Slf4j
 @Configuration
 @EnableConfigurationProperties(LoggingProperties.class)
