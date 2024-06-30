7e7f4c3272fa7260e1f76e0fe5557bbed758b5f8
Ilya Pukhov <dinamond2003@gmail.com>
2024-06-30 09:16:07 +0300

    victoria-metrics and grafana

--- user-service/src/main/java/com/ilyap/userservice/client/TaskServiceClient.java
+++ user-service/src/main/java/com/ilyap/userservice/client/TaskServiceClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "task-service", url = "localhost:8082/api/v1/tasks")
+@FeignClient(name = "task-service", url = "http://localhost:8082/api/v1/tasks")
 public interface TaskServiceClient {
 
     @GetMapping("/{taskId:\\d+}")
