1af699b28d9ca81be54a4bbad5a3f22e83c1b1f6
Ilya Pukhov <dinamond2003@gmail.com>
2024-07-01 16:43:57 +0300

    fix configs path

--- user-service/src/main/java/com/ilyap/userservice/client/TaskServiceClient.java
+++ user-service/src/main/java/com/ilyap/userservice/client/TaskServiceClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "task-service", url = "lb://task-service/api/v1/tasks")
+@FeignClient(name = "task-service", url = "http://task-service/api/v1/tasks")
 public interface TaskServiceClient {
 
     @GetMapping("/{taskId:\\d+}")
